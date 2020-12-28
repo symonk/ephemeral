92f143dc385461e4c341a17aa661ed2717fda272
symonk <jackofspaces@gmail.com>
2020-12-28 12:04:24 +0000

    adding a --target param; upgrading developer dependencies; bump flake to 120

--- ephemeral/cli.py
+++ ephemeral/cli.py
@@ -6,8 +6,12 @@ import sys
 def main():
     """Console script for ephemeral."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("")
+    parser.add_argument("--target", action="store", required=True, destination="target")
     return 0
+
+
+def ascii() -> None:
+    ...
 
 
 if __name__ == "__main__":
