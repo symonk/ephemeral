84681ca84ddf00b29952be374168708e664e09ce
symonk <jackofspaces@gmail.com>
2020-12-28 19:06:06 +0000

    fix spacing; colours of report hook

--- ephemeral/core/ephemeral_report.py
+++ ephemeral/core/ephemeral_report.py
@@ -1,6 +1,8 @@
 from typing import Optional
 from typing import Sequence
 
+from colorama import Fore
+
 from ephemeral import Configuration
 from ephemeral import ephemeral_hookimpl
 
@@ -9,4 +11,6 @@ from ephemeral import ephemeral_hookimpl
 def ephemeral_report(config: Configuration, ports: Sequence[Optional[int]]) -> None:
     config.terminal_writer.announce_report()
     for port in ports:
-        print(f"Port: {port} was detected as open...")
+        print(
+            f" **** Port: {Fore.GREEN + port + Fore.RESET} is open and potentially {Fore.RED} vulnerable! {Fore.RESET}"
+        )
