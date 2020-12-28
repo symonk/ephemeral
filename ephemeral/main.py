b257e86c64f4666175a96bc6d1ea3204a7ba47c2
symonk <jackofspaces@gmail.com>
2020-12-28 19:25:02 +0000

    tidy up spacing

--- ephemeral/main.py
+++ ephemeral/main.py
@@ -97,7 +97,7 @@ def welcome() -> None:
 |  _| | '_ \| '_ \ / _ \ '_ ` _ \ / _ \ '__/ _` | |
 | |___| |_) | | | |  __/ | | | | |  __/ | | (_| | |
 |_____| .__/|_| |_|\___|_| |_| |_|\___|_|  \__,_|_|
-   |_|
+      |_|
 {Fore.RESET} =================================================== {Fore.WHITE}
   Ephemeral: Powerful python port scanner, https://github.com/symonk/ephemeral
     """,
