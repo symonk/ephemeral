68beb61ac336e8c0f13d3f98305d0fa6d6e09acb
symonk <jackofspaces@gmail.com>
2020-12-28 12:08:31 +0000

    adding welcome ascii raw strings to output; tidying up --target

--- ephemeral/cli.py
+++ ephemeral/cli.py
@@ -5,13 +5,26 @@ import sys
 
 def main():
     """Console script for ephemeral."""
+    welcome()
     parser = argparse.ArgumentParser()
-    parser.add_argument("--target", action="store", required=True, destination="target")
+    parser.add_argument("--target", action="store", required=True, dest="target")
     return 0
 
 
-def ascii() -> None:
-    ...
+def welcome() -> None:
+    print(
+        r"""
+ ===================================================
+  _____       _                                   _
+ | ____|_ __ | |__   ___ _ __ ___   ___ _ __ __ _| |
+ |  _| | '_ \| '_ \ / _ \ '_ ` _ \ / _ \ '__/ _` | |
+ | |___| |_) | | | |  __/ | | | | |  __/ | | (_| | |
+ |_____| .__/|_| |_|\___|_| |_| |_|\___|_|  \__,_|_|
+       |_|
+  ===================================================
+  Ephemeral: Powerful python port scanner
+    """
+    )
 
 
 if __name__ == "__main__":
