d22829da5ddcb93986e2771c481fc959ab0b3142
symonk <jackofspaces@gmail.com>
2021-01-10 14:40:07 +0000

    fix sphinx package pathing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../zonic/'))
+sys.path.insert(0, os.path.abspath('../../zonic'))
 
 project = 'zonic'
 copyright = '2021, Simon Kerr'
